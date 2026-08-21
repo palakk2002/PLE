885a1972561eb6f08fcc0e5910b817301bd6b952
Palak Patel <palakk2103@gmail.com>
2026-08-21 14:41:33 +0530

    Fix import file casing for Linux server

--- backend/src/modules/vendor/controllers/gstSettings.controller.js
+++ backend/src/modules/vendor/controllers/gstSettings.controller.js
@@ -1,5 +1,5 @@
-import { Product } from '../../../models/product.model.js';
-import { Category } from '../../../models/category.model.js';
+import { Product } from '../../../models/Product.model.js';
+import { Category } from '../../../models/Category.model.js';
 import { ApiResponse } from '../../../utils/ApiResponse.js';
 import { asyncHandler } from '../../../utils/asyncHandler.js';
 import { ApiError } from '../../../utils/ApiError.js';
